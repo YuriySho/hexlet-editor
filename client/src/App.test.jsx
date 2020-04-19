2d84bc00be2c74a3131995b0b842c1b39d13082c
greybutton <greybutton@yandex.com>
2020-04-19 11:23:25 +0400

    remove create-react-app

--- client/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders page', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/header/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- client/src/features/index.js
+++
@@ -1,4 +0,0 @@
-import Editor from './Editor';
-
-// eslint-disable-next-line import/prefer-default-export
-export { Editor };
