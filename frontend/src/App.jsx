bc0e92d78b07953503f475421417e3d02f1b3e60
frontstall <deepspiral@yandex.ru>
2020-05-10 11:48:07 +0300

    add bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Editor } from './features';
 
 const App = () => (
-  <>
-    <header>header</header>
+  <div className="d-flex flex-column min-vh-100">
+    <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+      <div className="container">header</div>
+    </nav>
     <Editor />
-    <footer>footer</footer>
-  </>
+    <footer className="footer pb-5 mt-auto">
+      <div className="container">
+        jopa
+      </div>
+    </footer>
+  </div>
 );
 
 export default App;
