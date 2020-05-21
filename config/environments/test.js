731e382f572e671759895fe3a86927b8a1ff2c0d
Vasiliy Kudinev <vk@didlogic.com>
2020-05-21 21:44:28 +0300

    env test add param name to db

--- config/environments/test.js
+++ config/environments/test.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
+import faker from 'faker';
 
 export default (app) => {
   app.config.logLevel = 'debug';
   app.config.cacheModules = true;
+  app.config.db.name = faker.random.uuid();
 };
